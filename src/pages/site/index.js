ef6f0282df5a015978d47f0edbeeecd228fb2998
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2020-09-13 11:02:46 -0300

    Atualização SEO HOME

--- src/pages/site/index.js
+++ src/pages/site/index.js
@@ -23,11 +23,11 @@ const Home = () => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <title> | Carpede</title>
-        <meta name="title" content="title" />
-        <meta name="author" content="author" />
-        <meta name="description" content="description" />
-        <meta name="keywords" content="keywords" />
+        <title>Seu App exclusivo de Delivery | Carpede</title>
+        <meta name="title" content="Sua empresa com um aplicativo exclusivo de Delivery" />
+        <meta name="author" content="http://wule.com.br/" />
+        <meta name="description" content="Seu aplicativo exclusivo de Delivery, sem comissões por pedido, sem concorrência e a melhor experiência em entregas com atualizações em tempo real." />
+        <meta name="keywords" content="aplicativo delivery, app delivery, aplicativo personalizado delivery, aplicativo de entregas, aplicativo, app, negócio, entregas, aplicativo para meu negócio, aplicativo para negócios" />
         <meta name="robots" content="all" />
 
         <meta name="geo.region" content="BR-SP" />
@@ -35,45 +35,45 @@ const Home = () => {
         <meta name="ICBM" content="geoLatitude, geoLongitude" />
 
         <meta name="language" content="pt-br" />
-        <meta name="copyright" content="nomeEmpresa." />
+        <meta name="copyright" content="Carpede - Aplicativos exclusivos para o delivery de seu negócio." />
         <meta name="distribution" content="global" />
         <meta name="audience" content="all" />
-        <meta name="url" content="canonical" />
-        <meta name="classNameification" content="ramo" />
-        <meta name="category" content="ramo" />
-        <meta name="Page-Topic" content="title - nomeEmpresa" />
+        <meta name="url" content="https://www.carpede.com/" />
+        <meta name="classNameification" content="applications" />
+        <meta name="category" content="applications" />
+        <meta name="Page-Topic" content="Seu App exclusivo de Delivery | Carpede" />
         <meta name="rating" content="general" />
         <meta name="fone" content="tel" />
-        <meta name="city" content="cidade" />
+        <meta name="city" content="Distrito Federal, Brasília" />
         <meta name="country" content="Brasil" />
-        <meta property="publisher" content="creditos" />
+        <meta property="publisher" content="Wule Agência Digital" />
 
-        <link rel="canonical" href="canonical" />
+        <link rel="canonical" href="https://www.carpede.com/" />
         <meta name="googlebot" content="all" />
         <meta name="geo.placename" content="Brasil" />
-        <meta name="geo.region" content="cidade" />
-        <meta name="name" content="nomeEmpresa" />
-        <meta name="image" content="url.$card" />
+        <meta name="geo.region" content="Distrito Federal, Brasília" />
+        <meta name="name" content="Carpede" />
+        <meta name="image" content="https://www.carpede.com/images/card.jpg" />
 
-        <meta name="twitter:card" content="url.$card" />
-        <meta name="twitter:url" content="canonical" />
-        <meta name="twitter:site" content="canonical" />
-        <meta name="twitter:title" content="title - nomeEmpresa" />
-        <meta name="twitter:description" content="description" />
-        <meta name="twitter:image" content="url.$card" />
+        <meta name="twitter:card" content="https://www.carpede.com/images/card.jpg" />
+        <meta name="twitter:url" content="https://www.carpede.com/" />
+        <meta name="twitter:site" content="https://www.carpede.com/" />
+        <meta name="twitter:title" content="Seu App exclusivo de Delivery | Carpede" />
+        <meta name="twitter:description" content="Seu aplicativo exclusivo de Delivery, sem comissões por pedido, sem concorrência e a melhor experiência em entregas com atualizações em tempo real." />
+        <meta name="twitter:image" content="https://www.carpede.com/images/card.jpg" />
 
-        <meta property="og:title" content="title - nomeEmpresa" />
+        <meta property="og:title" content="Seu App exclusivo de Delivery | Carpede" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="canonical" />
-        <meta property="og:site_name" content="nomeEmpresa" />
-        <meta property="og:author" content="nomeEmpresa" />
+        <meta property="og:url" content="https://www.carpede.com/" />
+        <meta property="og:site_name" content="Carpede" />
+        <meta property="og:author" content="Carpede" />
         <meta property="og:locale" content="pt_BR" />
         <meta property="og:region" content="Brasil" />
-        <meta property="og:image" content="card" />
+        <meta property="og:image" content="https://www.carpede.com/images/card.jpg" />
         <meta property="og:image:type" content="image/jpg" />
         <meta property="og:image:width" content="250" />
         <meta property="og:image:height" content="250" />
-        <meta property="og:description" content="description" />
+        <meta property="og:description" content="Seu aplicativo exclusivo de Delivery, sem comissões por pedido, sem concorrência e a melhor experiência em entregas com atualizações em tempo real." />
 
         <link rel="icon" href="source/images/favicon.png" type="image/x-icon" />
         <link rel="shortcut icon" type="image/x-icon" href="source/images/favicon.png" />
